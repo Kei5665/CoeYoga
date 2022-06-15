77b050b9348e04509cd9d0cd89d4c9da450e6208
Kei <kt10195665@yahoo.ne.jp>
2022-06-15 21:58:36 +0900

    Add: フォームからcreateアクションに接続するところまで作成 #5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
+  protect_from_forgery
+
   def index
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    render json: { url: root_path }
   end
 end

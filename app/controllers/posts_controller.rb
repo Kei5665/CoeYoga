2a0cc33bc060c7ed9a7a3da7dfa8e707700257b9
Kei <kt10195665@yahoo.ne.jp>
2022-06-19 00:03:43 +0900

    Add: コンソールではapiの値を取れた #7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
   end
  
   def create
+
     @post = Post.new(posts_params)
 
     if @post.save
@@ -17,7 +18,27 @@ class PostsController < ApplicationController
       render :new
     end
 
+    @post = Post.new(posts_params)
+
+    audio = "/Users/kei/Desktop/coeyoga/public/" + @post.audio.url
+    connection = Faraday.new(url: 'https://api.webempath.net/v2/analyzeWav') do |f|
+      f.request :multipart
+      f.request :url_encoded
+      f.response :logger
+      f.adapter Faraday.default_adapter
+    end
+
+    response = connection.post do |req|
+      req.body = {
+        'apikey': 'vUOfl-btNyJdBwt7Iml-AlmlCnADZOZm93NUwzgrg8s',
+        'wav': Faraday::Multipart::FilePart.new(audio, 'audio/wav')
+      }
+    end
+
+    render json: { url: root_path }
   end
+
+  private
 
   def posts_params
     params.permit(:title, :body, :audio)
